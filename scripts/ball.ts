e8e05d43f0fc5a6bf9823052bd5e0ec8bbe8d39f
yuan tan <dyxtan@gmail.com>
2021-05-30 16:45:06 +1200

    add Ball class

--- scripts/ball.ts
+++ scripts/ball.ts
@@ -1,5 +1,17 @@
-const hello = (name: string) => {
-  console.log('hello', name);
-}
+let initialId = 1;
+class Ball {
+  readonly id: number;
+  x: number;
+  y: number;
+  radius: number;
+  rotation: number;
+  velocity: { vX: number, vY: number };
+  selected: boolean;
+  img: string;
 
-hello('bob');
+  constructor() {
+    this.id = initialId;
+    initialId++;
+  }
+
+}
