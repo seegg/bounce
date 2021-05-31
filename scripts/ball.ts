2c1bb9de1ad40284f550870102cc56d2ee37caca
yuan tan <dyxtan@gmail.com>
2021-05-31 21:43:01 +1200

    add comment for Ball class property

--- scripts/ball.ts
+++ scripts/ball.ts
@@ -7,11 +7,26 @@ class Ball {
   rotation: number;
   velocity: { vX: number, vY: number };
   selected: boolean;
+  // The img associated with an instance of Ball
   img: string;
 
-  constructor() {
+  constructor(x: number, y: number, r: number, imgSrc: string, selected: boolean = false) {
     this.id = initialId;
     initialId++;
+    this.x = x;
+    this.y = y;
+    this.radius = r;
+    this.img = imgSrc;
+    this.selected = selected;
+  }
+
+  // Update the position of the ball after every frame
+  updatePosition(gravity: number, deceleration: number,): void {
+
+  }
+
+  reverseDirection(distance: number) {
+
   }
 
 }
