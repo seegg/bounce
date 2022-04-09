89aa92d7763080c494f89fb2c48d178b08a1e1c5
yuan tan <dyxtan@gmail.com>
2022-04-09 22:43:53 +1200

    update github pages bundle

--- scripts/index.ts
+++ scripts/index.ts
@@ -3,10 +3,18 @@ import { setUpImages } from "./images";
 import { modalInit } from "./modal";
 import { insertBounceElements } from './add-controls';
 
-insertBounceElements();
-setUpImages();
-modalInit();
-init();
+function setUp() {
+  insertBounceElements();
+  setUpImages();
+  modalInit();
+  init();
+}
+
+// setTimeout(() => {
+//   setUp();
+// }, 1000);
+
+setUp();
 
 
 
